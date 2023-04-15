bc84db8ae8e718348ef40874b082983c6568f8cd
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2023-04-15 16:11:52 -0400

    modificacion

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
-function EncriptarAndDesencriptar() {
+document.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('.form__input');
   const btnEncriptar = document.querySelector('.btn__encriptar');
   const btnDesencriptar = document.querySelector('.btn_desencriptar');
+  const btnCopy = document.querySelector('.btn__text');
   const textEncriptado = document.querySelector('.textarea');
-  const muneco = document.querySelector('.muneco')
-  const text_textarea = document.querySelector('.text_textarea')
+  const muneco = document.querySelector('.muneco');
+  const text_textarea = document.querySelector('.text_textarea');
 
   function encriptar() {
-    const regex = /^[a-zA-Z]+$/;
+    const regex = /^[a-zA-Z]/;
     if (!regex.test(input.value)) {
       alert('El texto solo puede contener caracteres alfabéticos.');
       return '';
     }
-  
+
     const textoEncriptado = input.value
       .toLowerCase()
       .trim()
@@ -38,16 +39,21 @@ function EncriptarAndDesencriptar() {
 
   function handleClickEncriptar(e) {
     e.preventDefault();
+    if (input.value === '') {
+      btnCopy.classList.add('text_textarea__display__none');
+    }
+    btnCopy.classList.remove('text_textarea__display__none');
     textEncriptado.innerHTML = encriptar();
-    muneco.classList.add('text_textarea__display__none')
-    text_textarea.classList.add('text_textarea__display__none')
+    muneco.classList.add('text_textarea__display__none');
+    text_textarea.classList.add('text_textarea__display__none');
+
     input.value = '';
   }
 
   function handleClickDesencriptar(e) {
     e.preventDefault();
     textEncriptado.innerHTML = desencriptar();
-    input.value = '';
+    input.innerHTML = '';
   }
 
   function handleKeyPress(e) {
@@ -56,10 +62,18 @@ function EncriptarAndDesencriptar() {
       alert('Precione Click en el boton encriptar');
     }
   }
-
+  async function copiarTexto(e) {
+    try {
+      e.preventDefault();
+      await navigator.clipboard.writeText(textEncriptado.value);
+      btnCopy.textContent = 'Texto copiado';
+      textEncriptado.value.innerHTML = '';
+    } catch (error) {
+      console.error('Error al copiar el texto: ', error);
+    }
+  }
+  btnCopy.addEventListener('click', copiarTexto);
   input.addEventListener('keydown', handleKeyPress);
   btnEncriptar.addEventListener('click', handleClickEncriptar);
   btnDesencriptar.addEventListener('click', handleClickDesencriptar);
-}
-
-document.addEventListener('DOMContentLoaded', EncriptarAndDesencriptar);
+});
